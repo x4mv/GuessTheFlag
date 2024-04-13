4077fde542e7c2181002163af3e92fef415fcb94
x4mv <contactsscu@gmail.com>
2024-04-12 20:39:38 -0400

    Trabajando con Botones y alertas

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,15 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var alertShown = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("this is a test 2 ")
+        ZStack{
+            VStack(spacing:0){
+                Color.red
+                Color.blue
+            }
+            
+            Button("There is a question", action: handleClick)
+            .foregroundStyle(.white)
+            
+            .alert("important message", isPresented: $alertShown) {
+                Button("OK", role: .destructive, action: handleOk)
+                Button("Cancel", role: .cancel) {}
+            }
+            
+                
         }
-        .padding()
-        .background(.red)
+        .ignoresSafeArea()
+        
+    }
+    
+    func handleClick(){
+        print("haz hecho click")
+        alertShown = true
+    }
+    
+    func handleOk(){
+        print("click in ok")
     }
         
 }
